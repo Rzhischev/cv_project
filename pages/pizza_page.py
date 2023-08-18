ab4f5fa9946eccc2d720150a0a7be2dd6bb1e35f
Grisha Rzhishchev <107999749+Rzhischev@users.noreply.github.com>
2023-08-18 13:40:10 +0300

    Update pizza_page.py

--- pages/pizza_page.py
+++ pages/pizza_page.py
@@ -13,25 +13,42 @@ def app():
     st.title("Модель для обнаружения пицц")
     st.write("Эта страница использует YOLO модель для обнаружения пицц на изображениях.")
     
+    # Upload image through file uploader
     uploaded_image = st.file_uploader("Загрузите изображение", type=["jpg", "jpeg", "png"])
     
+    # Input box for user to enter image URL
+    image_url = st.text_input("Или введите URL изображения", "")
+    
+    # Check if an image is uploaded
     if uploaded_image is not None:
         # Read the image from uploaded file
         image = Image.open(uploaded_image)
-        
-        # Display the original image
-        st.image(image, caption='Загруженное изображение', use_column_width=True)
-        
-        # Perform inference with YOLO model
-        results = model(image, conf=0.4)
+        # Process and display the image
+        process_image(image)
 
-        # Show the results
-        for r in results:
-            im_array = r.plot()  # plot a BGR numpy array of predictions
-            im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-            st.image(im, caption='Результат обнаружения', use_column_width=True)
+    # Check if a URL is entered
+    elif image_url:
+        # Download the image from the URL
+        response = requests.get(image_url)
+        image = Image.open(BytesIO(response.content))
+        # Process and display the image
+        process_image(image)
+        
+def process_image(image):
+    # Display the original image
+    st.image(image, caption='Загруженное изображение', use_column_width=True)
+        
+    # Perform inference with YOLO model
+    results = model(image, conf=0.4)
+
+    # Show the results
+    for r in results:
+        im_array = r.plot()  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        st.image(im, caption='Результат обнаружения', use_column_width=True)
 
 # Run the Streamlit app
 if __name__ == "__main__":
     app()
+
 
