c2e1807650b812180cc4a41f31d6519721f92ba4
Grisha Rzhishchev <107999749+Rzhischev@users.noreply.github.com>
2023-08-18 13:44:50 +0300

    Update yolo8_page.py

--- pages/yolo8_page.py
+++ pages/yolo8_page.py
@@ -1,36 +1,53 @@
 import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
+import requests
+from io import BytesIO
 
 # Load the YOLO model
-# CONFIG_PATH = 'yolov8n-seg.yaml'  # Path to your YAML configuration file
 WEIGHTS_PATH = './pages/windmill.pt'  # Path to your weights file
-# model = YOLO(CONFIG_PATH)
 model = YOLO(WEIGHTS_PATH)
 
 def app():
-    st.title("Модель для обнаружения ветряков")
-    st.write("Эта страница использует YOLO модель для обнаружения ветряков на изображениях.")
+    st.title("Модель обнаружения ветряков")
+    st.write("Эта страница использует модель YOLOv8 для обнаружения ветряков на изображениях.")
     
+    # Upload image through file uploader
     uploaded_image = st.file_uploader("Загрузите изображение", type=["jpg", "jpeg", "png"])
     
+    # Input box for user to enter image URL
+    image_url = st.text_input("Или введите URL изображения", "")
+    
+    # Check if an image is uploaded
     if uploaded_image is not None:
         # Read the image from uploaded file
         image = Image.open(uploaded_image)
-        
-        # Display the original image
-        st.image(image, caption='Загруженное изображение', use_column_width=True)
-        
-        # Perform inference with YOLO model
-        results = model(image)
+        # Process and display the image
+        process_image(image)
 
-        # Show the results
-        for r in results:
-            im_array = r.plot()  # plot a BGR numpy array of predictions
-            im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-            st.image(im, caption='Результат обнаружения', use_column_width=True)
+    # Check if a URL is entered
+    elif image_url:
+        # Download the image from the URL
+        response = requests.get(image_url)
+        image = Image.open(BytesIO(response.content))
+        # Process and display the image
+        process_image(image)
+        
+def process_image(image):
+    # Display the original image
+    st.image(image, caption='Загруженное изображение', use_column_width=True)
+        
+    # Perform inference with YOLO model
+    results = model(image)
+
+    # Show the results
+    for r in results:
+        im_array = r.plot()  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        st.image(im, caption='Результат обнаружения', use_column_width=True)
 
 # Run the Streamlit app
 if __name__ == "__main__":
     app()
+
 
