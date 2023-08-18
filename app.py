39f248660e5f2391c8a29ad4a93c8ab44568457f
Grisha Rzhishchev <107999749+Rzhischev@users.noreply.github.com>
2023-08-18 13:28:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ st.set_page_config(
 st.sidebar.header("Home page")
 c1, c2 = st.columns(2)
 
-c2.image('https://s41256.pcdn.co/wp-content/uploads/2019/04/SLIDER-Appen_image_annotation_05.jpg')
+c2.image('https://kartinkof.club/uploads/posts/2023-05/1683610960_kartinkof-club-p-vetryak-kartinki-44.jpg')
 
 c1.markdown("""
-# Проект по Компьютерному зрению
-### Cостоит из 3 частей:
- ##### 1. Детекция ингридиентов пиццы с помощью YOLOv5
- ##### 2. Детекция опухулей мозга по фотографии с помощью YOLOv5
+## Компьютерное зрение • Computer Vision
+### Проект:
+ ##### 1. Детекция ингридиентов пиццы с помощью YOLOv8
+ ##### 2. Детекция ветряков с помощью YOLOv8
  ##### 3. Очищение документов от шумов с помощью автоэнкодера 
 """)
