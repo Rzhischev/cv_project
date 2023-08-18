d92f2df49ae95a41c38dbe2795a0ffadb700f8a8
Grisha Rzhishchev <107999749+Rzhischev@users.noreply.github.com>
2023-08-18 12:32:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import streamlit as st
-import windmill_page
-import pizza_page
-import encoder_page
+from pages import encoder_page, pizza_page, yolo8_page
 
+# Define the pages in the app
 PAGES = {
-    "Модель Windmill": windmill_page,
-    "Модель Pizza": pizza_page,
-    "Модель Encoder": encoder_page
+    "Encoder Page": encoder_page,
+    "Pizza Page": pizza_page,
+    "YOLO8 Page": yolo8_page
 }
 
-st.sidebar.title('Навигация')
-selection = st.sidebar.radio("Перейти к странице:", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+def main():
+    st.sidebar.title("Navigation")
+    choice = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+    # Render the page selected by the user
+    selected_page = PAGES[choice]
+    selected_page.app()
+
+# Run the app
+if __name__ == "__main__":
+    main()
